47a285ca3fbe8574435ff0502b5da5264c46ce17
zm-musti <mustafa.oeztuerk@schule-zukunftsmotor.org>
2025-05-23 13:42:23 +0200

    v1

--- max_mustermann.py
+++ max_mustermann.py
@@ -10,4 +10,6 @@ hobby = "Lesen"
 charaktereigenschaften = "freundlich"
 
 print(f"{name} ist ein {alter} Jahre alter {geschlecht}er {beruf}, der {größe} groß ist, {gewicht} wiegt, {augenfarbe}e Augen und {haarfarbe}e Haare hat. "
-      f"In seiner Freizeit beschäftigt er sich gerne mit {hobby} und gilt als {charaktereigenschaften}.")
+      f"In seiner Freizeit beschäftigt er sich gerne mit {hobby} und gilt als {charaktereigenschaften}.
+
+      #das ist ein Kommentar
