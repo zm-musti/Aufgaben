e94f2cb976c6170081e865d4793c88f5290e3167
zm-musti <mustafa.oeztuerk@schule-zukunftsmotor.org>
2025-05-23 13:29:05 +0200

    änderung

---
+++ max_mustermann.py
@@ -0,0 +1,13 @@
+name = "Max Mustermann"
+alter = 30
+geschlecht = "männlich"
+größe = "1,80m"
+gewicht = "75kg"
+augenfarbe = "braun"
+haarfarbe = "schwarz"
+beruf = "Softwareentwickler"
+hobby = "Lesen"
+charaktereigenschaften = "freundlich"
+
+print(f"{name} ist ein {alter} Jahre alter {geschlecht}er {beruf}, der {größe} groß ist, {gewicht} wiegt, {augenfarbe}e Augen und {haarfarbe}e Haare hat. "
+      f"In seiner Freizeit beschäftigt er sich gerne mit {hobby} und gilt als {charaktereigenschaften}.")
