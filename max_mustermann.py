f207d14b57ef0efc75de14bf8fc812d981cb80f0
zm-musti <mustafa.oeztuerk@schule-zukunftsmotor.org>
2025-05-26 10:00:45 +0200

    v1

--- max_mustermann.py
+++ max_mustermann.py
@@ -12,4 +12,4 @@ charaktereigenschaften = "freundlich"
 print(f"{name} ist ein {alter} Jahre alter {geschlecht}er {beruf}, der {größe} groß ist, {gewicht} wiegt, {augenfarbe}e Augen und {haarfarbe}e Haare hat. "
       f"In seiner Freizeit beschäftigt er sich gerne mit {hobby} und gilt als {charaktereigenschaften}.
 
-      #das ist ein Kommentar
+      #das ist ein Kommentar 
